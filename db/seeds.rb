b614fa6ee83febd49b5457b3146d079c94a363ff
Agustin Camino <agustin.camino@gmail.com>
2015-10-12 13:26:04 -0500

    Add data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Ticket.create([
+  {
+    name: 'Charles Holdsworth',
+    phone_number: '+14153674129',
+    description: 'I played for Middlesex in the championships and my mallet squeaked the whole time! I demand a refund!'
+  },
+  {
+    name: 'John Woodger',
+    phone_number: '+15712812415',
+    description: 'The mallet you sold me broke! Call me immediately!'
+  },
+])
