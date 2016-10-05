dbb4a3a2ee8d2503c5b0397154cffe1ea1ba009e
Mario Celi <mcelicalderon@gmail.com>
2016-10-05 15:07:30 -0500

    Use twilio-ruby next-gen library

--- lib/twilio_capability.rb
+++ lib/twilio_capability.rb
@@ -4,7 +4,7 @@ class TwilioCapability
     # https://www.twilio.com/user/account
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     auth_token  = ENV['TWILIO_AUTH_TOKEN']
-    capability = Twilio::Util::Capability.new account_sid, auth_token
+    capability = Twilio::JWT::Capability.new(account_sid, auth_token)
 
     application_sid = ENV['TWIML_APPLICATION_SID']
     capability.allow_client_outgoing application_sid
