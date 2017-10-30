e6183f431db197661d76fc7e7a4a05add8dadf0f
Tomer <tomer@shvueli.com>
2017-10-30 17:47:18 -0400

    fix Twilio in-browser-call functionality using application_sid

--- lib/twilio_capability.rb
+++ lib/twilio_capability.rb
@@ -4,17 +4,17 @@ class TwilioCapability
   def self.generate(role)
     # To find TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN visit
     # https://www.twilio.com/user/account
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token  = ENV['TWILIO_AUTH_TOKEN']
+    account_sid     = ENV['TWILIO_ACCOUNT_SID']
+    auth_token      = ENV['TWILIO_AUTH_TOKEN']
+    application_sid = ENV['TWIML_APPLICATION_SID']
     # binding.pry
     capability = Twilio::JWT::ClientCapability.new(account_sid, auth_token)
-    outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(account_sid, role)
+    outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(application_sid, role)
     capability.add_scope outgoing_scope
 
     incoming_scope = Twilio::JWT::ClientCapability::IncomingClientScope.new(role)
     capability.add_scope incoming_scope
 
-    application_sid = ENV['TWIML_APPLICATION_SID']
 
     capability.to_s
   end
