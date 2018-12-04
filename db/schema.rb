78d25cf1524a8bb08babd3f81a783b5ae18d54dc
7kaji <hirotonbuu@gmail.com>
2018-12-04 22:14:28 +0900

    set the PostgreSQL as the default database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2015_10_12_161241) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tickets", force: :cascade do |t|
     t.string "name", null: false
     t.string "phone_number", null: false
