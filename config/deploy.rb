95fad8606bada402885aae6a35b3d445e4ddf302
Brad Smith <brad@smithwebtek.com>
2020-03-26 14:09:47 -0400

    Change .env to config/.env

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/deploy/poc"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key", ".env"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key", "config/.env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
