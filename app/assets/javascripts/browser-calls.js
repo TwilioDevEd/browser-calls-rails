78bb211908f7209419b8be6e42155028b08178cf
Brad Smith <brad@smithwebtek.com>
2020-03-26 14:07:48 -0400

    Add .env to linked_files

--- app/assets/javascripts/browser-calls.js
+++ app/assets/javascripts/browser-calls.js
@@ -19,7 +19,6 @@ function updateCallStatus(status) {
 $(document).ready(function () {
     console.log('document ready loaded')
   $.post("/token/generate", {page: window.location.pathname}, function(data) {
-    debugger;
     console.log("We have a token!! ", data.token)
     userGesture(data.token);
   });
