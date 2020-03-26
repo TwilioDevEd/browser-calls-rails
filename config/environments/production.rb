462b9d9c15fde8a1e1fc7f0484ea1c0e306ac71e
Brad Smith <brad@smithwebtek.com>
2020-03-26 10:28:25 -0400

    Change confid.assets.js_compressor in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,10 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  # https://github.com/lautis/uglifier/issues/127
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+  
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
