c6b298b4b0aae5ce4e76ce266e861ed9aab6d1ea
Brad Smith <brad@smithwebtek.com>
2020-03-26 13:17:23 -0400

    Remove old pry

--- lib/twilio_capability.rb
+++ lib/twilio_capability.rb
@@ -7,14 +7,13 @@ class TwilioCapability
     account_sid     = ENV['TWILIO_ACCOUNT_SID']
     auth_token      = ENV['TWILIO_AUTH_TOKEN']
     application_sid = ENV['TWIML_APPLICATION_SID']
-    # binding.pry
+
     capability = Twilio::JWT::ClientCapability.new(account_sid, auth_token)
     outgoing_scope = Twilio::JWT::ClientCapability::OutgoingClientScope.new(application_sid, role)
     capability.add_scope outgoing_scope
 
     incoming_scope = Twilio::JWT::ClientCapability::IncomingClientScope.new(role)
     capability.add_scope incoming_scope
-
 
     capability.to_s
   end
